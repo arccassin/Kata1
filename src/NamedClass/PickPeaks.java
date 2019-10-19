b195f97868b21cde37a8925d80442dbc9b11a8b0
Emessar_WinPC <arccassin@gmail.com>
2019-10-19 14:19:08 +0300

    add new kata10

---
+++ src/NamedClass/PickPeaks.java
@@ -0,0 +1,11 @@
+package NamedClass;
+import java.util.Map;
+import java.util.List;
+
+public class PickPeaks {
+
+    public static Map<String,List<Integer>> getPeaks(int[] arr) {
+        // Your code here!
+        return null;
+    }
+}
